360f6067b4ab99f74dae864744d9e476d6ecc35f
Nicolas Arrastia <nicolasarrastia04@gmail.com>
2025-06-01 16:51:50 -0300

    feat: Landing Page to legal study (example)

---
+++ src/content/serviceCard.js
@@ -0,0 +1,30 @@
+export const legalServices = [
+  {
+    title: "Enfermedades Profesionales",
+    description:
+      "Realizamos telegramas ante las ART a fin de que se te pague la indemnización que te corresponde por las patologías sufridas durante la relación laboral.",
+    icon: "/doctor.svg", // reemplazá por el ícono real
+    link: "#consultar",
+  },
+  {
+    title: "Accidentes de Trabajo",
+    description:
+      "Te asesoramos sobre el tratamiento médico brindado por la ART e iniciamos trámite para cobrar la indemnización en la COMISIÓN MÉDICA.",
+    icon: "/tractor.svg",
+    link: "#consultar",
+  },
+  {
+    title: "Jubilación",
+    description:
+      "Realizamos liquidación laboral gratuita en virtud a tus recibos de sueldo y tu antigüedad en tus puestos de trabajo.",
+    icon: "/mail.svg",
+    link: "#consultar",
+  },
+  {
+    title: "Accidentes de Tránsito",
+    description:
+      "Reclamamos ante la aseguradora del tercero para obtener la indemnización por daños y lesiones.",
+    icon: "/car.svg",
+    link: "#consultar",
+  },
+];
---
+++ src/pages/api/contact.ts
@@ -0,0 +1,41 @@
+export const prerender = false;
+
+import type { APIRoute } from "astro";
+import nodemailer from "nodemailer";
+
+export const POST: APIRoute = async ({ request }) => {
+  const { name, mail, message } = await request.json();
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: import.meta.env.EMAIL_USER,
+      pass: import.meta.env.EMAIL_PASS,
+    },
+  });
+
+  try {
+    await transporter.sendMail({
+      from: import.meta.env.EMAIL_USER,
+      to: import.meta.env.EMAIL_USER,
+      subject: `Nuevo Mensaje Desde La WEB de: ${name}`,
+      replyTo: mail,
+      text: message,
+    });
+
+    return new Response(JSON.stringify({ success: true }), {
+      status: 200,
+    });
+  } catch (err) {
+    console.error("Error al enviar el mensaje:", err);
+    return new Response(
+      JSON.stringify({
+        success: false,
+        message: "Error interno al enviar el email.",
+      }),
+      {
+        status: 500,
+      }
+    );
+  }
+};
